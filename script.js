373976cf8a36fe529e1f421558a20408ac763603
Shreya <shreyadikundwar@gmail.com>
2026-04-16 12:06:54 +0530

    local storage conflict added

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const form = document.getElementById("contactForm");
 const formNote = document.getElementById("formNote");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-/* <!-- test --> */
+  event.preventDefault()
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const phone = document.getElementById("phone").value.trim();
@@ -14,7 +12,6 @@ form.addEventListener("submit", (event) => {
     formNote.style.color = "#fca5a5";
     return;
   }
-/* <!-- test --> */
 
   formNote.textContent = "Thanks! Your inspection request has been received. We�ll contact you soon.";
   formNote.style.color = "#86efac";
