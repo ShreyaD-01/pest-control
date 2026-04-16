6242450fa5696de7381323cbbbc552d7189ca98d
Shreya <shreyadikundwar@gmail.com>
2026-04-16 12:52:19 +0530

    solved

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ const form = document.getElementById("contactForm");
 const formNote = document.getElementById("formNote");
 
 form.addEventListener("submit", (event) => {
-<<<<<<< HEAD
   event.preventDefault()
-=======
   event.preventDefault();
 // test aditya
->>>>>>> 6784daab6b64151c802d9417a50dc7cf2d0f4d84
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   // test aditya
@@ -18,13 +15,8 @@ form.addEventListener("submit", (event) => {
     formNote.style.color = "#fca5a5";
     return;
   }
-<<<<<<< HEAD
-
-  formNote.textContent = "Thanks! Your inspection request has been received. We�ll contact you soon.";
-=======
 // test aditya
   formNote.textContent = "Thanks! Your inspection request has been received. We’ll contact you soon.";
->>>>>>> 6784daab6b64151c802d9417a50dc7cf2d0f4d84
   formNote.style.color = "#86efac";
   form.reset();
 });
